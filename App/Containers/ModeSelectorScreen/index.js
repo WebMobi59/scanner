3d19a6575b8c52d126181e6b6fa3b208b71303ef
autumnblue <vinichn7@gmail.com>
2019-04-29 13:10:53 +0300

    re-implement mode-selector-screen
    create customNavBar, cross-device dimension and refactor modeSelectorScreen.js

--- App/Containers/ModeSelectorScreen/index.js
+++ App/Containers/ModeSelectorScreen/index.js
@@ -1,91 +1,75 @@
-import React, { PureComponent } from 'react'
-import { View, Text } from 'react-native'
-import { compose, withPropsOnChange } from 'recompose'
-import { get as _get } from 'lodash'
-import * as Animatable from 'react-native-animatable'
-import { Metrics, Images, Colors, Fonts, ApplicationStyles } from '@Themes'
-
-import {
-    Wrapper,
-    ImageWrapper,
-    BarcodeImage,
-    WelcomeTextWrapper,
-    WelcomeText,
-    SelectText,
-    SelectOption,
-    SelectImageWrapper,
-    OptionImage,
-    DescriptionWrapper,
-    UpText,
-    BottomText,
-    ArrowImageWrapper,
-    ArrowImage,
-    ExplanationText
-} from './styles'
+import React, {Component} from 'react';
+import { TouchableOpacity } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import {compose, withPropsOnChange} from 'recompose';
+import {get as _get} from 'lodash';
+import {Metrics, Images, Colors, Fonts, ApplicationStyles} from '@Themes';
+import styles from './styles';
+import * as scale from '../../Utils/Scale';
 
 // React Apollo
-import { withAuth, withCreateAccount, withLogin } from '@GraphQL/Account/decorators'
+import {withAuth, withCreateAccount, withLogin} from '@GraphQL/Account/decorators'
 
-class ModeSelectorScreen extends PureComponent {
+class ModeSelectorScreen extends Component {
 
-  handleEnterPhotoMode = () => {
-    this.props.navigation.navigate('PartnerSelectorScreen', { transition: 'card' })
-  }
+    handleEnterPhotoMode = () => {
+        this.props.navigation.navigate('PartnerSelectorScreen', {transition: 'card'})
+    };
 
-  handleEnterRunnerMode = () => {
-    this.props.navigation.navigate('PartnerSelectorScreen', { transition: 'card' })
-  }
+    handleEnterRunnerMode = () => {
+        this.props.navigation.navigate('PartnerSelectorScreen', {transition: 'card'})
+    };
 
-  render() {
+    render() {
 
-    return (
-      <Wrapper>
-        <ImageWrapper>
-          <BarcodeImage source={Images.barcodeBlack} />
-        </ImageWrapper>
-        <WelcomeTextWrapper>
-          <WelcomeText>Welcome to the Digitization App</WelcomeText>
-        </WelcomeTextWrapper>
-        <SelectText>Which best describes you?</SelectText>
-        <SelectOption>
-          <SelectImageWrapper width={39} height={32}>
-            <OptionImage source={Images.camera} />
-          </SelectImageWrapper>
-          <DescriptionWrapper>
-            <UpText>I’m Taking Photos</UpText>
-            <BottomText>Enter Photo Mode</BottomText>
-          </DescriptionWrapper>
-          <ArrowImageWrapper>
-            <ArrowImage source={Images.arrowRight} />
-          </ArrowImageWrapper>
-        </SelectOption>
-        <SelectOption>
-          <SelectImageWrapper width={35} height={48}>
-            <OptionImage source={Images.checkProduct} />
-          </SelectImageWrapper>
-          <DescriptionWrapper>
-            <UpText>I’m Pulling Products from Shelves</UpText>
-            <BottomText>Enter Runner Mode</BottomText>
-          </DescriptionWrapper>
-          <ArrowImageWrapper>
-            <ArrowImage source={Images.arrowRight} />
-          </ArrowImageWrapper>
-        </SelectOption>
-        <ExplanationText>You’ll be able to change this later</ExplanationText>
-      </Wrapper>
-    )
-  }
+        return (
+            <Animatable.View style={styles.container}>
+                <Animatable.View style={styles.imageWrapper}>
+                    <Animatable.Image source={Images.barcodeBlack} style={styles.barCode}/>
+                </Animatable.View>
+                <Animatable.View style={styles.welcomeTextWrapper}>
+                    <Animatable.Text style={styles.welcomeText}>Welcome to the Digitization App</Animatable.Text>
+                </Animatable.View>
+                <Animatable.Text style={styles.selectText}>Which best describes you?</Animatable.Text>
+                <TouchableOpacity style={styles.selectOption}>
+                    <Animatable.View style={[{width: 39 * scale.widthRatio, height: 32 * scale.heightRatio}]}>
+                        <Animatable.Image source={Images.camera} style={styles.option}/>
+                    </Animatable.View>
+                    <Animatable.View>
+                        <Animatable.Text style={styles.upText}>I’m Taking Photos</Animatable.Text>
+                        <Animatable.Text style={styles.bottomText}>Enter Photo Mode</Animatable.Text>
+                    </Animatable.View>
+                    <Animatable.View style={styles.arrowImageWrapper}>
+                        <Animatable.Image source={Images.arrowRight} style={styles.arrow} />
+                    </Animatable.View>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.selectOption}>
+                    <Animatable.View style={[{width: 35 * scale.widthRatio, height: 48 * scale.heightRatio}]}>
+                        <Animatable.Image source={Images.checkProduct} style={styles.option}/>
+                    </Animatable.View>
+                    <Animatable.View>
+                        <Animatable.Text style={styles.upText}>I’m Pulling Products from Shelves</Animatable.Text>
+                        <Animatable.Text style={styles.bottomText}>Enter Runner Mode</Animatable.Text>
+                    </Animatable.View>
+                    <Animatable.View style={styles.arrowImageWrapper}>
+                        <Animatable.Image source={Images.arrowRight} style={styles.arrow} />
+                    </Animatable.View>
+                </TouchableOpacity>
+                <Animatable.Text style={styles.explanationText}>You’ll be able to change this later</Animatable.Text>
+            </Animatable.View>
+        )
+    }
 }
 
 const enhance = compose(
-  withAuth,
-  withPropsOnChange(
-    (props, nextProps) =>
-      _get(props, 'auth.session.isAuthenticated', false) !== _get(nextProps, 'auth.session.isAuthenticated', false),
-    ({ auth }) => ({ isAuthenticated: _get(auth, 'session.isAuthenticated', false) })
-  ),
-  withLogin,
-  withCreateAccount
-)
+    withAuth,
+    withPropsOnChange(
+        (props, nextProps) =>
+            _get(props, 'auth.session.isAuthenticated', false) !== _get(nextProps, 'auth.session.isAuthenticated', false),
+        ({auth}) => ({isAuthenticated: _get(auth, 'session.isAuthenticated', false)})
+    ),
+    withLogin,
+    withCreateAccount
+);
 
 export default enhance(ModeSelectorScreen)
--- App/Containers/ModeSelectorScreen/styles.js
+++ App/Containers/ModeSelectorScreen/styles.js
@@ -1,109 +1,97 @@
-import styled from 'styled-components/native'
-import { Metrics, Fonts } from '@Themes'
+import { StyleSheet } from 'react-native';
+import { Metrics, Fonts } from '@Themes';
+import * as scale from '../../Utils/Scale';
 
-export const Wrapper = styled.View`
-    position: relative;
-    align-items: center;
-    height: ${Metrics.screenHeight};
-`;
-
-export const ImageWrapper = styled.View`
-    width: 29px;
-    height: 28px;
-    margin-top: 41px;
-`;
-
-export const BarcodeImage = styled.Image`
-    width: 100%;
-    height: 100%;
-`;
-
-export const WelcomeTextWrapper = styled.View`
-    margin: 0 100px;
-    flex-direction: row;
-`;
-
-export const WelcomeText = styled.Text`
-    flex: 1;
-    flex-wrap: wrap;
-    margin-top: 14px;
-    color: #1f2952;
-    font-size: 24px;
-    font-weight: 700;
-    letter-spacing: -0.7;
-    font-family: ${Fonts.type.bold};
-    text-align: center;
-`;
-
-export const SelectText = styled.Text`
-    margin-top: 15px;
-    margin-bottom: 34px;
-    color: #1f2952;
-    font-family: ${Fonts.type.base};
-    font-weight: 400;
-    font-size: 16px;
-`;
-
-export const SelectOption = styled.TouchableOpacity`
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    width: 320px;
-    height: 106px;
-    margin-bottom: 25px;
-    padding: 0 24px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.12);
-    border-radius: 4px;
-    border: 1px solid #eaeaea;
-    background-color: #ffffff;
-`;
-
-export const SelectImageWrapper = styled.View`
-    width: ${props => props.width};
-    height: ${props => props.height};
-`;
-
-export const OptionImage = styled.Image`
-    width: 100%;
-    height: 100%;
-`;
-
-export const DescriptionWrapper = styled.View`
-
-`;
-
-export const UpText = styled.Text`
-    width: 150px;
-    margin-top: 2px;
-    color: #1f2952;
-    font-family: ${Fonts.type.bold};
-    font-size: 16px;
-    font-weight: 700;
-`;
-
-export const BottomText = styled.Text`
-    color: #1f2952;
-    font-family: ${Fonts.type.base};
-    font-size: 12px;
-    font-weight: 400;
-`;
-
-export const ArrowImageWrapper = styled.View`
-    width: 7px;
-    height: 14px;
-`;
-
-export const ArrowImage = styled.Image`
-    width: 100%;
-    height: 100%;
-`;
-
-export const ExplanationText = styled.Text`
-    position: absolute;
-    color: #1f2952;
-    font-family: ${Fonts.type.base};
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 20px;
-    bottom: 73;
-`;
+export default StyleSheet.create({
+    container: {
+        position: 'relative',
+        alignItems: 'center',
+        height: Metrics.screenHeight
+    },
+    imageWrapper: {
+        width: 29 * scale.widthRatio,
+        height: 28 * scale.widthRatio,
+        marginTop: 41 * scale.heightRatio
+    },
+    barCode: {
+        width: '100%',
+        height: '100%'
+    },
+    welcomeTextWrapper: {
+        marginHorizontal: 100 * scale.widthRatio,
+        flexDirection: 'row'
+    },
+    welcomeText: {
+        flex: 1,
+        flexWrap: 'wrap',
+        marginTop: 14 * scale.heightRatio,
+        color: '#1f2952',
+        fontSize: 24 * scale.widthRatio,
+        fontWeight: '700',
+        letterSpacing: -0.7,
+        fontFamily: Fonts.type.bold,
+        textAlign: 'center'
+    },
+    selectText: {
+        marginTop: 15 * scale.heightRatio,
+        marginBottom: 34 * scale.heightRatio,
+        color: '#1f2952',
+        fontFamily: Fonts.type.base,
+        fontWeight: '400',
+        fontSize: 16 * scale.widthRatio
+    },
+    selectOption: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: 320 * scale.widthRatio,
+        height: 106 * scale.heightRatio,
+        marginBottom: 25 * scale.heightRatio,
+        paddingHorizontal: 24 * scale.widthRatio,
+        shadowColor: 'rgba(0, 0, 0, 0.12)',
+        shadowOffset: {
+            width: 2,
+            height: 0
+        },
+        shadowRadius: 8 * scale.heightRatio,
+        borderRadius: 4 * scale.heightRatio,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#eaeaea'
+    },
+    option: {
+        width: '100%',
+        height: '100%'
+    },
+    upText: {
+        width: 150 * scale.widthRatio,
+        marginTop: 2 * scale.heightRatio,
+        color: '#1f2952',
+        fontFamily: Fonts.type.bold,
+        fontSize: 16 * scale.widthRatio,
+        fontWeight: '700'
+    },
+    bottomText: {
+        color: '#1f2952',
+        fontFamily: Fonts.type.base,
+        fontSize: 12 * scale.widthRatio,
+        fontWeight: '400'
+    },
+    arrowImageWrapper: {
+        width: 7 * scale.widthRatio,
+        height: 14 * scale.widthRatio
+    },
+    arrow: {
+        width: '100%',
+        height: '100%'
+    },
+    explanationText: {
+        position: 'absolute',
+        color: '#1f2952',
+        fontFamily: Fonts.type.base,
+        fontSize: 12 * scale.widthRatio,
+        fontWeight: '400',
+        lineHeight: 20 * scale.heightRatio,
+        bottom: 73 * scale.heightRatio
+    }
+});
